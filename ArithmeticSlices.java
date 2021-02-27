2361761228c0d205bcea527da03adf36853d80cf
MansiKukreja24 <55954224+MansiKukreja24@users.noreply.github.com>
2021-02-27 23:18:58 +0530

    Update and rename ArithmeticSlices.py to ArithmeticSlices.java

---
+++ ArithmeticSlices.java
@@ -0,0 +1,13 @@
+public class Solution{
+  public int numberOfArithmeticSlices(int[] A){
+    int count=0;
+    for(int i=0;i<A.length-2;i++){
+      int diff=A[i+1]-A[i];
+      for(int j=i+2;j<A.length;j++){
+        if(A[j]-A[j-1]==diff)
+          count++;
+        else
+          break;
+      }
+    }
+    return count;
